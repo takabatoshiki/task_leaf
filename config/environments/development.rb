f9a8c5d22428addc97c61b9b65d47aefe535b6f0
高場利樹 <takajoutoshiki@takabarijunoMacBook-Air-2.local>
2019-02-23 21:29:57 +0900

    implement credential

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # config.logger.formatter = proc { |severity, timestamp, progname, message|
+  #   "#{timestamp} :#{severity}: #{message}\n"
+  # }
 end
